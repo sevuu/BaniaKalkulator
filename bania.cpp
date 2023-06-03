8fc4b05326e3c111fba3e84bda979946e9d286cd
sevuu <rodihasan1@gmail.com>
2023-06-03 11:22:15 +0200

    poggers

--- bania.cpp
+++ bania.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 
 struct STrunek{
-    char nazwa[64];
+	string nazwa;
     float ml;
     float procent,cena;
+	float spejson;
+	float etanol;
     void wczytaj();
-    float etanol();
-    float spejson();
+    void etanolowanie();
+    void spejsonowanie();
 };
 
 struct SLista{
@@ -29,33 +31,40 @@ struct SLista{
     }
 };
 
+void STrunek::spejsonowanie(){
+    if(ml!=500.0){
+        float podzial = 500.0/ml;
+        procent = procent/podzial;
+    }
+    float wspolczynnik = procent/cena;
+    spejson = wspolczynnik;
+}
+
+void STrunek::etanolowanie(){
+    float realprocent = procent/100;
+    float ilealko = realprocent * ml;
+    if(ml!=500.0){					// nie mam pojęcia dlaczego muszę to sprawdzać, ale z jakiegos powodu trzeba
+        float podzial = 500.0/ml;
+        ilealko = ilealko*podzial;
+    }
+    etanol = ilealko;
+}
+
 void STrunek::wczytaj(){
     cout<<"nazwa: ";
     cin.ignore();
-    cin.getline(nazwa,64);
+    getline(cin,nazwa);
     cout<<"ml: ";
     cin>>ml;
     cout<<"woltaż: ";
     cin>>procent;
     cout<<"cena: ";
     cin>>cena;
+	spejsonowanie();
+	etanolowanie();
 };
 
-float STrunek::etanol(){
-    // obliczanie ile jest czystego alkoholu w trunku
-    float realprocent = procent/100;
-    float ilealko = realprocent * ml;
-    return ilealko;
-}
 
-float STrunek::spejson(){
-    if(ml!=500.0){
-        float podzial = 500.0/ml;
-        procent = procent/podzial;
-    }
-    float wspolczynnik = procent/cena;
-    return wspolczynnik;
-}
 
 void SLista::wczytaj(){
     cout<<"Liczba trunków do porównania: ";
@@ -70,24 +79,20 @@ void SLista::wczytaj(){
 void SLista::wypisz(){
     cout<<"==========Wyniki==========="<<endl;
     for(int i = 0; i<liczba;i++){
-        cout<<"Ilosc etanolu w trunku "<<piwo[i].nazwa<<": "<<piwo[i].etanol()<<"ml"<<endl;
-        cout<<"Współczynnik spejsona: "<<piwo[i].spejson()<<endl;
+        cout<<"Ilosc etanolu w trunku "<<piwo[i].nazwa<<": "<<piwo[i].etanol<<"ml"<<endl;
+        cout<<"Współczynnik spejsona: "<<piwo[i].spejson<<endl;
         cout<<endl;
     }
-
 }
 
 string SLista::najoplacalniejsze(){
-    float tmp = piwo[0].spejson();
-    string tmpnazwa;
-    for(int i = 0; i<liczba; i++){
-        if(piwo[i].spejson()>tmp){
-            tmp = piwo[i].spejson();
-            string nazwastring(piwo[i].nazwa);
-            tmpnazwa = nazwastring;
-        }
-    }
-    return tmpnazwa;
+	STrunek tmp = piwo[0];
+	for(int i = 0; i<liczba; i++){
+		if(piwo[i].spejson>tmp.spejson){
+			tmp = piwo[i];
+		}
+	}
+	return tmp.nazwa;
 }
 
 int main(int argc, const char** argv){
